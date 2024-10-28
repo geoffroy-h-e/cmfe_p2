d0cdc51fe924e3adaa0deba66d0cfb840722b0b1
geoffroy-h-e <geoffroy.hebert-emond@hec.ca>
2024-10-27 22:10:47 -0400

    update on code
    - added sources for data in subfolder
    - working routine with 10 factors
    - update on pdf with what is done

--- main.m
+++ main.m
@@ -1,21 +1,39 @@
 
-% load in the data
+%% load the data
+
+% daily_returns 
 xlsx_daily_data = 'daily_data.xlsx';
-daily_data = readtable(xlsx_daily_data);
-xlsx_daily_factors = 'daily_factors.xlsx';
-daily_factors = readtable(xlsx_daily_factors);
+daily_data = readtable(xlsx_daily_data); 
 
-dates = daily_data.Caldt;
-r = daily_data.Dret;  % Mutual funds returns
-F = daily_factors{:, 2:end};  % Benchmark/factor returns (T x m)
+%fama and french factors
+xlsx_daily_fama5_factors = 'daily_fama5_factors.xlsx';
+daily_fama5_factors = readtable(xlsx_daily_fama5_factors); 
 
-% Ensure returns align by date, trim if needed
+% hou xue q5 factors model
+xlsx_daily_q5_factors = 'daily_q5_factors.csv';
+daily_q5_factors = readtable(xlsx_daily_q5_factors);
+
+%set up the number of observations to test different time frames 
+n = 1000;
+dates = daily_data.Caldt(1:n);
+r = daily_data.Dret(1:n);
+
+%remove dates and combine in a single matrix
+Fama5 = daily_fama5_factors{1:n, 2:end};  % Fama-French 5-factors
+Q5 = daily_q5_factors{1:n, 2:end};   % Q5-factors
+F = [Fama5, Q5];
+
+% Ensure returns align 
 T = length(r);  % Number of time periods
 m = size(F, 2);  % Number of factors
 
-% Calculate intermediate terms
-r_norm_sq = norm(r)^2;  % ||r||^2
-r_mean_sum_sq = (sum(r))^2;  % (1^T r)^2
+%% correlation matrix to check if some factors have an alarming correlation 
+
+corr_matrix = corr(F);
+disp('Correlation Matrix of Factors:');
+disp(corr_matrix);
+
+%% setting up the optimization problem 
 
 % Hessian matrix
 I = eye(T);
@@ -28,13 +46,11 @@ f = -2 * ((r' * F) / T - (sum(r) / T^2) * (ones_T' * F));
 % Constraints: sum(w) = 1, w >= 0
 Aeq = ones(1, m);  % Equality constraint: sum(w) = 1
 beq = 1;
-A = -eye(m);  % Inequality constraints: w >= 0
+A = -eye(m);  % Inequality constraints: w over or equal to 0 
 b = zeros(m, 1);
 
-%lb = zeros(m, 1);  % Lower bounds: 0
-%ub = ones(m, 1) * 0.3; test with upper bounds to see if we are stuck 
-
-% Run the quadratic programming solver
+% Run the quadratic programming solver, 
+% added tweak on tolerance to avoid getting stuck with a 1 weight 
 options = optimoptions('quadprog', 'Display', 'off', 'TolFun', 1e-9, 'TolX', 1e-9);
 [w_opt, ~] = quadprog(H, f', A, b, Aeq, beq, [], [], [], options);
 %[w_opt, ~] = quadprog(H, f', A, b, Aeq, beq, lb, ub, x0, options);
@@ -44,6 +60,3 @@ uP = r - F * w_opt;  % Residuals
 
 w_opt
 
-corr_matrix = corr(F);
-disp('Correlation Matrix of Factors:');
-disp(corr_matrix);
